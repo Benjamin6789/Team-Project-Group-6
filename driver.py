474c525ae80975eb038fb581c685ca169682181e
Benjamin_Helfer_ <95501241+Benjamin6789@users.noreply.github.com>
2025-04-03 15:03:18 +0100

    updated dunctions

--- driver.py
+++ driver.py
@@ -1,39 +1,59 @@
+#from SEES import Sensors
+from datetime import datetime
 from SENSORS import Sensors
 import time
+import csv
+import os
 
-sensors = Sensors()
+if __name__ == "__main__":
+	sensors = Sensors()
 
-secconds_counter:int = 0
-while True:
-	try:
-		start_time:float = time.monotonic()
-		secconds_counter += 1
-		
-		print("soil moisture", sensors.soil_moisture() , "%") # out of 100% 
-		print("temp", sensors.temp())		  	# 22.63
-		print("nh3", sensors.nh3())				# 204.38
-		
-		
-		if secconds_counter % 5 == 0: # executes evrey 5 seconds
-			print("humidity", sensors.humidity()) 	# 79.19
-			print("pressure", sensors.pressure()) 	# 677.44
-		
-		if secconds_counter % 10 == 0: # executes evrey 10 seconds
-			print("light", sensors.light())		  	# 15.4
-			print("oxidising", sensors.oxidising()) # 19030.45
-			print("reducing", sensors.reducing())	# 1249.07
-			
-			secconds_counter:int = 0
-			
-		print()
-		
+	fields:list = ["Time", "Soil Moisture", "Temperature", "Nh3", "Humidity", "Pressure", "Light", "Oxidizing", "Reducing"] 
+	data_collection_inteval:float = 0.1 # time in secconds
+
+	cache = []
+	file_name = "Data.csv"
+
+	if not os.path.isfile(file_name):
+		with open(file_name, "w+") as f:
+			write = csv.writer(f)
+			write.writerow(fields)
+			write.writerow([])
+	
+	
+	start = time.time()
+	while True:
 		try:
-			time_to_sleep = 1.0 - ((time.monotonic() - start_time) % 60.0) # 1 seccond compensated sleep time
-			time.sleep(time_to_sleep)
+			data = []
+			
+			start_time:float = time.monotonic()
+			data.append(datetime.now().strftime('%d-%H:%M:%S.%f'))
+			data.append(sensors.soil_moisture())
+			data.append(sensors.temp())
+			data.append(sensors.nh3())
+			data.append(sensors.humidity())			
+			data.append(sensors.pressure())
+			data.append(sensors.light())
+			data.append(sensors.oxidising())
+			data.append(sensors.reducing())
+			
+			cache.append(data)
+
+			if len(cache) == 15:
+				with open("Data.csv", "a") as f:
+					write = csv.writer(f)
+					write.writerows(cache)
+				cache = []
+
+			try:
+				time_to_sleep = data_collection_inteval - ((time.monotonic() - start_time) % 60.0) # 1 seccond compensated sleep time
+				elapsed_time = time.time() - start
+				
+				time.sleep(time_to_sleep)
+
+			except KeyboardInterrupt:
+				exit()
 			
 		except KeyboardInterrupt:
 			exit()
-		
-	except KeyboardInterrupt:
-		exit()
 	
